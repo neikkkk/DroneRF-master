cc335d0f52f7e61ac3bac2bc24f3ac57f36cea34
neikkkk <144123260+neikkkk@users.noreply.github.com>
2023-12-21 11:10:02 +0800

    Update Demo_1_Database_details.m

--- Demo_1_Database_details.m
+++ Demo_1_Database_details.m
@@ -1,32 +1,3 @@
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
-%
-% Copyright 2019 Mohammad Al-Sa'd
-%
-% Licensed under the Apache License, Version 2.0 (the "License");
-% you may not use this file except in compliance with the License.
-% You may obtain a copy of the License at
-%
-%     http://www.apache.org/licenses/LICENSE-2.0
-%
-% Unless required by applicable law or agreed to in writing, software
-% distributed under the License is distributed on an "AS IS" BASIS,
-% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-% See the License for the specific language governing permissions and
-% limitations under the License.
-%
-% Authors: Mohammad F. Al-Sa'd (mohammad.al-sad@tuni.fi)
-%          Amr Mohamed         (amrm@qu.edu.qa)
-%          Abdulla Al-Ali
-%          Tamer Khattab
-%
-% The following reference should be cited whenever this script is used:
-%     M. Al-Sa'd et al. "RF-based drone detection and identification using
-%     deep learning approaches: an initiative towards a large open source
-%     drone database", 2019.
-%
-% Last Modification: 12-02-2019
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
-
 close all; clear; clc
 filename = 'D:\Data\';  % Path of raw RF data
 format shortg
